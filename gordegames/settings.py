99e163eea650c16c019f9564b3ad0ab8130469ae
andresousadb <andresousadb@gmail.com>
2024-04-01 14:29:03 -0300

    Imagens na nuvem

--- gordegames/settings.py
+++ gordegames/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
 
     # my apps
@@ -28,7 +29,7 @@ INSTALLED_APPS = [
     'orders',
     'django_filters',
     'cloudinary',
-    'cloudinary_storage'
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
